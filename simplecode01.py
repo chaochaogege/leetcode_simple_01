154549dcef473848c1c138601e821059eedfbabe
chaochaogege <50255988+chaochaogege@users.noreply.github.com>
2019-05-04 11:24:42 +0800

    Update simplecode01.py

--- simplecode01.py
+++ simplecode01.py
@@ -1,10 +1,17 @@
-#coding=utf-8
-def twototle(nums,totle):
-    for i in range(len(nums)):
-        j=i+1
-        for j in range(len(nums)):
-            if (nums[i]+nums[j]==totle):
-                return i,j
-
+#coding=utf8
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            j=i+1
+            for j in range(len(nums)):
+                if (i!=j):
+                    if (nums[i]+nums[j]==target):
+                        return i,j
+                    
 if __name__ == '__main__':
     print(twototle([1, 4, 3, 5, 2], 7))
